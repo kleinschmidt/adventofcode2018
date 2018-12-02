1e16c5e6e47d5a5d56dec703f3dc017b43de0efe
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-01 21:41:33 -0500

    use a BitSet

--- 01.jl
+++ 01.jl
@@ -24,3 +24,30 @@ end
 @test [+7, +7, -2, -7, -4] |> star2 == 14
 
 @show star2(input)
+
+
+# can maybe use a BitSet to speed things up (represents a set of integers with a
+# bitstring).
+function star2bitset(input)
+    seen = BitSet([0])
+    freq = 0
+    for i in Iterators.cycle(input)
+        freq += i
+        if freq ∈ seen
+            return freq
+        else
+            push!(seen, freq)
+        end
+    end
+end
+
+@test [+1, -1] |> star2bitset == 0
+@test [+3, +3, +4, -2, -4] |> star2bitset == 10
+@test [-6, +3, +8, +5, -6] |> star2bitset == 5
+@test [+7, +7, -2, -7, -4] |> star2bitset == 14
+
+star2bitset(input)
+
+using BenchmarkTools
+@btime star2(input)             # 3.8 ms
+@btime star2bitset(input)       # 301 μs
