39313d49e1a7723c9ba2776221a815839702476f
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-03 16:44:57 -0500

    add generator/first version with benchmarking

--- 03.jl
+++ 03.jl
@@ -10,6 +10,23 @@ for (id, xrange, yrange) in input
     cloth[xrange, yrange] .+= 1
 end
 
+# part 1: number of squares with >1 claim
 @show sum(c > 1 for c in cloth)
 
-[id for (id, xrange, yrange) in input if all(isequal(1), view(cloth, xrange, yrange))]
+# part 2: find only claim that has no intersection
+# (re-use the "cloth" marked up for the first star to make this O(n))
+
+@btime [id
+        for (id, xrange, yrange)
+        in input
+        if all(isequal(1), view(cloth, xrange, yrange))]
+#   452.265 μs (5281 allocations: 185.72 KiB)
+# 1-element Array{Int64,1}:
+#  724
+
+@btime first(id
+             for (id, xrange, yrange)
+             in input
+             if all(isequal(1), view(cloth, xrange, yrange)))
+#   244.011 μs (2899 allocations: 101.86 KiB)
+# 724
