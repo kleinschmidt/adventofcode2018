7bbc0e33bda2b017d89061ef641278b59b2d4277
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-06 09:59:06 -0500

    hacky star 1 day 6

---
+++ 06.jl
@@ -0,0 +1,51 @@
+input = [(parse.(Int, split(pair, ','))..., ) for pair in readlines("06.input")]
+
+# strategy: brute force.  find closest point for each grid cell
+xs = 1:maximum(first.(input))+50
+ys = reshape(1:maximum(last.(input))+50, 1, :)
+
+input3d = reshape(input, 1, 1, :)
+
+xd = abs.(xs .- first.(input3d))
+yd = abs.(ys .- last.(input3d))
+
+abs_diff(a,b) = a > b ? a-b : b-a
+@btime distmap = abs.(xs .- first.(input3d)) .+ abs.(ys .- last.(input3d));
+@btime abs_diff.(xs, first.(input3d)) .+ abs_diff.(ys, last.(input3d));
+
+mindist, closest = findmin(abs.(xs .- first.(input3d)) .+ abs.(ys .- last.(input3d)); dims=3)
+last.(convert.(Tuple, closest))
+
+# the issue with that solution is there's no easy way to detect ties...
+
+function star1(input)
+    xs = 1:maximum(first.(input))+50
+    ys = 1:maximum(last.(input))+50
+    closest = zeros(Int, length(xs), length(ys))
+    for x in xs, y in ys
+        mindist = typemax(Int)
+        for i in 1:length(input)
+            dist = abs(input[i][1]-x) + abs(input[i][2]-y)
+            closest[x,y] = dist == mindist ? 0 : dist < mindist ? i : closest[x,y]
+            mindist = min(dist, mindist)
+        end
+    end
+    closest
+end
+
+@btime closest = star1(input);
+
+counts = zeros(Int, size(input))
+for i in closest
+    i > 0 && (counts[i] += 1)
+end
+
+dq = union(Set(closest[1:end, 1]),
+           Set(closest[1:end, end]),
+           Set(closest[1, 1:end]),
+           Set(closest[end, 1:end]))
+delete!(dq, 0)
+
+counts[[dq...]] .= 0
+
+findmax(counts)
