9de70f49cca5ea84990d7d78d1a35a0e8f247643
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-06 22:34:09 -0500

    full, slightly lazier solution

--- 06.jl
+++ 06.jl
@@ -2,60 +2,43 @@ input = [(parse.(Int, split(pair, ','))..., ) for pair in readlines("06.input")]
 
 # strategy: brute force.  find closest point for each grid cell
 xs = 1:maximum(first.(input))+50
-ys = reshape(1:maximum(last.(input))+50, 1, :)
+ys = 1:maximum(last.(input))+50
 
-input3d = reshape(input, 1, 1, :)
+manhattan_dist(a,b) = sum(abs.(a.-b))
 
-xd = abs.(xs .- first.(input3d))
-yd = abs.(ys .- last.(input3d))
+function findmin_noties(xs, tie_sentinel=0)
+    minx = first(xs)
+    mini = 0
+    for (i,x) in enumerate(xs)
+        mini = x > minx ? mini : x == minx ? tie_sentinel : i
+        minx = min(x, minx)
+    end
+    mini
+end
 
-abs_diff(a,b) = a > b ? a-b : b-a
-@btime distmap = abs.(xs .- first.(input3d)) .+ abs.(ys .- last.(input3d));
-@btime abs_diff.(xs, first.(input3d)) .+ abs_diff.(ys, last.(input3d));
-
-mindist, closest = findmin(abs.(xs .- first.(input3d)) .+ abs.(ys .- last.(input3d)); dims=3)
-last.(convert.(Tuple, closest))
-
-# the issue with that solution is there's no easy way to detect ties...
+function count_map(xs, n)
+    counts = zeros(Int, n)
+    for x in xs
+        x > 0 && (counts[x] += 1)
+    end
+    counts
+end
 
 function star1(input)
     xs = 1:maximum(first.(input))+50
     ys = 1:maximum(last.(input))+50
-    closest = zeros(Int, length(xs), length(ys))
-    for x in xs, y in ys
-        mindist = typemax(Int)
-        for i in 1:length(input)
-            dist = abs(input[i][1]-x) + abs(input[i][2]-y)
-            closest[x,y] = dist == mindist ? 0 : dist < mindist ? i : closest[x,y]
-            mindist = min(dist, mindist)
-        end
-manhattan_dist(a,b) = sum(abs.(a.-b))
-@btime distmap = abs.($xs .- first.($input3d)) .+ abs.($ys .- last.($input3d));
-@btime distmap2 = manhattan_dist.(tuple.($xs, $ys), $input3d);
-
-
-
-closest = last.(Tuple.(last(findmin(distmap2, dims=3))))
-closest_rev = size(distmap2,3)+1 .-
-    last.(Tuple.(last(findmin(view(distmap2, :, :, 50:-1:1), dims=3))))
-closest[closest .!= closest_rev] .= 0
-    end
-    closest
+    closest = [findmin_noties(manhattan_dist((x,y), i) for i in input) for x in xs, y in ys]
+    dq = union(Set(closest[1:end, 1]),
+               Set(closest[1:end, end]),
+               Set(closest[1, 1:end]),
+               Set(closest[end, 1:end]))
+    delete!(dq, 0)
+    counts[collect(dq)] .= 0
+    findmax(counts)
 end
 
-@btime closest = star1(input);
-
-counts = zeros(Int, size(input))
-for i in closest
-    i > 0 && (counts[i] += 1)
+function star2(input)
+    xs = 1:maximum(first.(input))+50
+    ys = 1:maximum(last.(input))+50
+    sum(sum(manhattan_dist((x,y), i) for i in input)<10000 for x in xs, y in ys)
 end
-
-dq = union(Set(closest[1:end, 1]),
-           Set(closest[1:end, end]),
-           Set(closest[1, 1:end]),
-           Set(closest[end, 1:end]))
-delete!(dq, 0)
-
-counts[[dq...]] .= 0
-
-findmax(counts)
