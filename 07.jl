b29d4a60df37c40d5fdd8b31c71fd3286d0944a6
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2018-12-09 22:44:34 -0500

    clean up 7

--- 07.jl
+++ 07.jl
@@ -15,16 +15,6 @@ Step F must be finished before step E can begin.
 
 input = parseline.(readlines("07.input"))
 
-# my feeling is that you can either start from the top or the bottom.  let's try
-# the top first: for each step, make a list of its dependents
-
-# idea here is to keep track of frontier as a sorted list.  then each time you
-# do a step, add all the steps it unlocks to the frontier.  the tricky thing is
-# that you still need to track the backward dependencies too to check if a step
-# is ready.
-
-# convert pairs into the forward edges (the vector of all the nodes that depend
-# on a given node)
 function forward(input)
     edges = Dict{Char,Vector{Char}}()
     for (before, after) in input
@@ -33,23 +23,11 @@ function forward(input)
     edges
 end
 
-# backward edges: all the steps that must be completed before a given step can
-# begin
 backward(input) = forward(reverse(i) for i in input)
 
-roots(input) = setdiff(keys(backward(input)), keys(forward(input)))
-
-# gonna work backwards, starting at roots (steps with no dependents).  give each
-# node a number: root is one.  all it's precursors are 2.  theirs are 3.  etc.
-# okayyyy but this doesn't address the fact that the sorting really depends on
-# the FORWARD direction (doing things when they first become available).
-#
-# the most straightforward way is to use a forward frontier and the backwards
-# edges to determine whether completing a task should add it to the frontier.
-
 mutable struct JobQueue
-    f_edges::Dict{Char}
-    b_edges::Dict{Char}
+    f_edges::Dict{Char,Vector{Char}}
+    b_edges::Dict{Char,Set{Char}}
     queue::Vector{Char}
 end
 
